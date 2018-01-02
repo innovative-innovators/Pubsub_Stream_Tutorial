2f57fdb525485e99a24e5963af67f41776969dff
vincentarthur <443519782@qq.com>
2018-01-02 14:30:45 +0800

    Add Window strategy of SupplementData PCollection

--- src/main/java/com/vc/pubsubdemo/PubsubDemo.java
+++ src/main/java/com/vc/pubsubdemo/PubsubDemo.java
@@ -59,7 +59,7 @@ public class PubsubDemo {
         Pipeline pipeline = Pipeline.create(myOptions);
 
         PCollection<String> suppData = pipeline.apply("Read Supplement Data", TextIO.read().from("gs://" + bucketName + "/TrainData_Fraud_gcpml.csv"))
-                .apply("FixedWindow",Window.<String>into(FixedWindows.of(Duration.millis(500))));
+                .apply("FixedWindow",Window.<String>into(FixedWindows.of(Duration.ZERO)));
 
         PCollection<String> input = pipeline
                 .apply("ReceiveMessage", PubsubIO.readStrings().fromTopic(topicName))
